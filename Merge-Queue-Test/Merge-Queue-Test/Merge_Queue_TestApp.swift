ba35082df6724ed9c57810fed0fcb6446d58733b
Jaron Lowe <j.crew100@yahoo.com>
2025-04-03 16:52:04 -0400

    - Xcode project upload - PR yml file.

---
+++ Merge-Queue-Test/Merge-Queue-Test/Merge_Queue_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Merge_Queue_TestApp.swift
+//  Merge-Queue-Test
+//
+//  Created by Jaron Lowe on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Merge_Queue_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
